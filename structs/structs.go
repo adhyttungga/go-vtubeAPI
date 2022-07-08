896385384038216bd56b9c8f3c1e9f03ab50e4e1
cap7nemo <adhytiatungga09031996@gmail.com>
2022-07-08 23:30:31 +0700

    fix user structs

--- structs/structs.go
+++ structs/structs.go
@@ -3,12 +3,12 @@ package structs
 import "time"
 
 type User struct {
-	Id          int      	`json:"id"`
+	Id          int      	`json:"-"`
 	Name        string    `json:"name" gorm:"size:100"`
 	Email       string    `json:"email" gorm:"size:100 unique"`
-	Password    []byte    `json:"-" gorm:"size:200"`
-	CreatedDate time.Time `json:"-" gorm:"type:timestamp;default:current_timestamp`
-	UpdatedDate time.Time `json:"-" gorm:"type:timestamp null`
+	Password    string    `json:"password" gorm:"size:200"`
+	CreatedDate time.Time `json:"-" gorm:"type:timestamp;default:current_timestamp"`
+	UpdatedDate time.Time `json:"-" gorm:"type:timestamp null"`
 }
 
 type Result struct {
