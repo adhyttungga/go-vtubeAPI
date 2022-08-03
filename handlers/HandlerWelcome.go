3758dd706b914c56a4ef4c3ad6d868c90574bb36
cap7_nemo <adhyttungga.jkt@gmail.com>
2022-08-03 22:34:06 +0700

    update

--- handlers/HandlerWelcome.go
+++ handlers/HandlerWelcome.go
@@ -2,47 +2,9 @@ package handlers
 
 import (
 	"fmt"
-	"log"
 	"net/http"
-
-	"github.com/adhyttungga/go-vtubeAPI/structs"
-	"github.com/dgrijalva/jwt-go"
 )
 
 func HomePage(w http.ResponseWriter, r *http.Request) {
-	c, err := r.Cookie("token");
-	log.Println(c)
-	
-	if err != nil {
-		if err == http.ErrNoCookie {
-			w.WriteHeader(http.StatusUnauthorized)
-			return
-		}
-
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	tokenString := c.Value
-	claims := &structs.Claims{}
-	jwtToken, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
-		return []byte("my_secret_key"), nil
-	})
-
-	if err != nil {
-		if err == jwt.ErrSignatureInvalid {
-			w.WriteHeader(http.StatusUnauthorized)
-			return
-		}
-		
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	if !jwtToken.Valid {
-		w.WriteHeader(http.StatusUnauthorized)
-		return
-	}
-
-	w.Write([]byte(fmt.Sprintf("Welcome %s!", claims.Username)))
+	fmt.Fprintf(w, "Welcome!")
 }
